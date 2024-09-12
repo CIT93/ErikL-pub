e46e254611d736330bb60e36f032b8706ca309df
Erik Lerma <eriklerma81@gmail.com>
2024-09-12 13:08:00 -0700

    setup and array of objects

--- w4/main.js
+++ w4/main.js
@@ -53,11 +53,24 @@ function start(houseHoldMembers, houseSize){
 
 function displayOutput() {
     for (array of cfpData){
+        const output = document.getElementById("output");
+        const newH2 = document.createElement("h2");
+        newH2.textContent = `Carbon Footprint ${array[4]}`;
+        const newH3 = document.createElement("h3");
+        newH3.textContent = `Based on number in and size of home`;
+        const newP = document.createElement("p");
+        newP.textContent = `This number is based on the number of people in the house of ${array[0]} (score: ${array[2]}),`;
+        newP.textContent += `and a ${array[1]} size of home (score:${array[3]}),`;
+        output.appendChild(newH2);
+        output.appendChild(newH3);
+        output.appendChild(newP);
+        /*
         console.log(array);
         const output = document.getElementById("output");
         const newP = document.createElement("p");
         newP.textContent = `You live in a ${array[1]} sized house which is worth ${array[3]} carbon footprint points and it occupies ${array[0]} people which is ${array[2]} carbon fooprint points which is a total of ${array[4]} total carbon footprint points `;
         output.appendChild(newP);
+        */
     }
 }
 
---
+++ w5/main.js
@@ -0,0 +1,101 @@
+const cfpData = [];
+
+function houseCalculation(houseSize) {
+
+    let houseSizePoints = 0;
+    if (houseSize === "apartment") {
+        houseSizePoints += 2;
+    } else if (houseSize === "small") {
+        houseSizePoints += 4;
+    } else if (houseSize === "medium") {
+        houseSizePoints += 7;
+    } else if (houseSize === "large") {
+        houseSizePoints += 10;
+    }
+
+    return houseSizePoints;
+}
+
+
+function pointsCalculation(numberInHousehold) {
+
+    let houseHoldPoints = 0;
+    if (numberInHousehold === 1) {
+        houseHoldPoints = 14;
+    } else if (numberInHousehold === 2) {
+        houseHoldPoints = 12;
+    } else if (numberInHousehold === 3) {
+        houseHoldPoints = 10;
+    } else if (numberInHousehold === 4) {
+        houseHoldPoints = 8;
+    } else if (numberInHousehold === 5) {
+        houseHoldPoints = 6;
+    } else if (numberInHousehold === 6) {
+        houseHoldPoints = 4;
+    } else if (numberInHousehold >= 7) {
+        houseHoldPoints = 2;
+    } else {
+        console.log("no upated to points");
+    }
+
+
+    return houseHoldPoints;
+}
+
+function displayRefactorObj(obj) {
+    const output = document.getElementById("output");
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `Carbon Footprint ${obj.carbonTotal}`;
+    const newH3 = document.createElement("h3");
+    newH3.textContent = `Based on number in and size of home`;
+    const newP = document.createElement("p");
+    newP.textContent = `This number is based on the number of people in the house of ${obj.houseMembers} (score: ${obj.housepts}),`;
+    newP.textContent += `and a ${obj.houseSize} size of home (score:${obj.houseSizepts}),`;
+    output.appendChild(newH2);
+    output.appendChild(newH3);
+    output.appendChild(newP);
+}
+
+function start(houseHoldMembers, houseSize) {
+    const housePoints = pointsCalculation(houseHoldMembers);
+    const houseSizePoints = houseCalculation(houseSize);
+    const total = housePoints + houseSizePoints;
+    
+
+    cfpData.push({
+        houseMembers: houseHoldMembers,
+        houseSize: houseSize,
+        housepts: housePoints,
+        houseSizepts: houseSizePoints,
+        carbonTotal: total
+    });
+
+   
+}
+
+function displayOutput() {
+    for (obj of cfpData) {
+        console.log(obj)
+        
+        const output = document.getElementById("output");
+        const newH2 = document.createElement("h2");
+        newH2.textContent = `Carbon Footprint ${obj.carbonTotal}`;
+        const newH3 = document.createElement("h3");
+        newH3.textContent = `Based on number in and size of home`;
+        const newP = document.createElement("p");
+        newP.textContent = `obj number is based on the number of people in the house of ${obj.houseMembers} (score: ${obj.housepts}),`;
+        newP.textContent += `and a ${obj.houseSize} size of home (score:${obj.houseSizePoints}),`;
+        output.appendChild(newH2);
+        output.appendChild(newH3);
+        output.appendChild(newP);
+        
+    }
+}
+
+start(2, "apartment");
+start(6, "medium");
+start(3, "large");
+start(7, "medium");
+start(5, "small");
+
+displayOutput();
