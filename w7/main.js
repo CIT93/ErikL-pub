e7c7b7c7396c44401470abbf5ed44eba0b8fb8c4
Erik Lerma <eriklerma81@gmail.com>
2024-09-26 18:17:46 -0700

    challenge 1

--- w7/main.js
+++ w7/main.js
@@ -1,51 +1,12 @@
 import { renderTbl } from "./render.js";
+import { houseCalculation, pointsCalculation  } from "./carbonfootprint.js";
 const cfpData = [];
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 
 
 
-function houseCalculation(houseSize) {
 
-    let houseSizePoints = 0;
-    if (houseSize === "apartment") {
-        houseSizePoints += 2;
-    } else if (houseSize === "small") {
-        houseSizePoints += 4;
-    } else if (houseSize === "medium") {
-        houseSizePoints += 7;
-    } else if (houseSize === "large") {
-        houseSizePoints += 10;
-    }
-
-    return houseSizePoints;
-}
-
-
-function pointsCalculation(numberInHousehold) {
-
-    let houseHoldPoints = 0;
-    if (numberInHousehold === 1) {
-        houseHoldPoints = 14;
-    } else if (numberInHousehold === 2) {
-        houseHoldPoints = 12;
-    } else if (numberInHousehold === 3) {
-        houseHoldPoints = 10;
-    } else if (numberInHousehold === 4) {
-        houseHoldPoints = 8;
-    } else if (numberInHousehold === 5) {
-        houseHoldPoints = 6;
-    } else if (numberInHousehold === 6) {
-        houseHoldPoints = 4;
-    } else if (numberInHousehold >= 7) {
-        houseHoldPoints = 2;
-    } else {
-        console.log("no upated to points");
-    }
-
-
-    return houseHoldPoints;
-}
 
 function displayRefactorObj(obj) {
     const output = document.getElementById("output");
