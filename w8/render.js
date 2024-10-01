377520eb24e1f1f973b1eb4f4a7ab688aa1fe412
Erik Lerma <eriklerma81@gmail.com>
2024-09-30 19:05:53 -0700

    my understanding

--- w8/render.js
+++ w8/render.js
@@ -13,29 +13,14 @@ function renderTbl(data){
         const tdName = document.createElement("td");
         tdName.textContent = obj.firstName;
        
-        const tdhouseMembers = document.createElement("td");
-        tdhouseMembers.textContent = obj.houseMembers;
-        
-        const tdhouseSize = document.createElement("td");
-        tdhouseSize.textContent = obj.houseSize;
-        
         const tdTotal = document.createElement("td");
         tdTotal.textContent = obj.carbonTotal;
 
         tr.appendChild(tdName);
-        tr.appendChild(tdhouseMembers);
-        tr.appendChild(tdhouseSize);
         tr.appendChild(tdTotal); 
         tbody.appendChild(tr)
 
-        const td = document.createElement("td");
-        const buttonEdit = document.createElement("button");
-        const buttonDel = document.createElement("button");
-        buttonDel.textContent = "Delete";
-        buttonEdit.textContent = "Edit";
-        td.appendChild(buttonEdit);
-        td.appendChild(buttonDel);
-        tr.appendChild(td);
+        
     });
 
     table.appendChild(tbody);
@@ -48,8 +33,9 @@ function  renderTblheading () {
     const table = document.createElement("table")
     const thead = document.createElement("thead")
     const tr = document.createElement("tr")
-    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
-    
+    //const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
+    const headingTextArr = ["Name", "Footprint"];
+
     headingTextArr.forEach(function(text){
         const th = document.createElement("th");
         th.textContent = text;
