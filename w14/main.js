4b0cdba48f1e8e793004bedb88500abe47068048
Erik Lerma <eriklerma81@gmail.com>
2024-11-14 01:09:13 -0800

    w14 my attempt at the code befire i review other students code or solution code

--- w14/main.js
+++ w14/main.js
@@ -1,28 +1,19 @@
-const url = 'https://banana.pudding'
-
-
-const request = new Request(url, {
-        headers: {
-            'Authorization' :  'Bearer 123'
-        }
-    })
-
+const url = 'https://jsonplaceholder.typicode.com/users'
 
 async function getData(){
-    try{
-      const response= await fetch(request)
-      const data = await response.json()  
-      if (response === 200){
-        console.log(`success`, data)
-      }else{
-        console.log(`Server Error`, data.error.message)
-      }
-      
-    } catch(error){
-        console.log(`Error`, error)
-    }
+    const response = await fetch(url)
+    const data = await response.json()
+    
+    const usernames = document.getElementById("usernames")
+    
+        data.forEach(user => {
+            
+            const usernameElement = document.createElement('p');
+            usernameElement.textContent = user.username;
+    
+            
+            usernames.appendChild(usernameElement);
+        })
 }
-   
 
 getData()
-
